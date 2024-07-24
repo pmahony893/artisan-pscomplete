17929ccacf61e4b00e3b2f1c0d1ba0cb5f374cab
pmahony893 <32841410+pmahony893@users.noreply.github.com.>
2024-07-24 09:51:06 +0100

    Make work for typing "php ./path/to/artisan"

--- completer.ps1
+++ completer.ps1
@@ -3,73 +3,80 @@ function php_artisan {
 }
 Set-Alias -Name art -Value php_artisan
 
-Register-ArgumentCompleter -CommandName art -Native -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
 
-    $commandPreElements = @()
-    $artFound = $false
-    foreach ($commandElement in $commandAst.CommandElements) {
+@('art', 'php') | ForEach-Object {
+    Register-ArgumentCompleter -CommandName $_ -Native -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+
+        $commandPreElements = @()
+        $artFound = $false
+        foreach ($commandElement in $commandAst.CommandElements) {
         
-        if ($commandElement.Extent.EndOffset -ge $cursorPosition) {
-            break
-        }
-
-        if (-not $artFound -and $commandElement -like 'art*') {
-            $artFound = $true
-        }
-        elseif ($artFound) {
-            $commandPreElements += $commandElement
-        }
-    }
-
-    if ($commandPreElements.Count -eq 0) {
-        php artisan --format=json | 
-            ConvertFrom-Json | 
-            Select-Object -ExpandProperty 'commands' | 
-            Where-Object { $_.name -like "$wordToComplete*" } |
-            ForEach-Object { 
-                [System.Management.Automation.CompletionResult]::new(
-                    $_.name,
-                    $_.name,
-                    'Command',
-                    $_.description)
+            if ($commandElement.Extent.EndOffset -ge $cursorPosition) {
+                break
             }
 
-        return
-    }
-    elseif ('--' -notin $commandPreElements) {
-        php artisan --format=json | 
-            ConvertFrom-Json | 
-            Select-Object -ExpandProperty 'commands' | 
-            Where-Object { $_.name -eq $commandPreElements[0] } | 
-            Select-Object -ExpandProperty 'definition' | 
-            Select-Object -ExpandProperty 'options' | 
-            ForEach-Object { $_.psobject.properties.Value } | 
-            ForEach-Object { 
-                $description = $_.description; 
-                @{
-                    name        = $_.name; 
-                    description = $description 
-                };
+            if (-not $artFound -and (Split-Path $commandElement -Leaf) -like 'art*') {
+                $artFound = $true
+            }
+            elseif ($artFound) {
+                $commandPreElements += $commandElement
+            }
+        }
 
-                $_.shortcut -split '\|' | 
-                    Where-Object { $_ -ne '' } | 
-                    ForEach-Object { 
-                        @{
-                            name        = $_; 
-                            description = $description 
-                        } 
-                    } 
-                } | 
-                Where-Object { $_.name -like "$wordToComplete*" } |
+        if (-not $artFound) {
+            return
+        }
+
+        if ($commandPreElements.Count -eq 0) {
+            php artisan --format=json | 
+                ConvertFrom-Json | 
+                Select-Object -ExpandProperty 'commands' | 
+                Where-Object { $_.hidden -ne $true -and $_.name -like "$wordToComplete*" } |
                 ForEach-Object { 
                     [System.Management.Automation.CompletionResult]::new(
                         $_.name,
                         $_.name,
-                        'ParameterName',
+                        'Command',
                         $_.description)
                 }
+
+                return
+            }
+            elseif ('--' -notin $commandPreElements) {
+                php artisan --format=json | 
+                    ConvertFrom-Json | 
+                    Select-Object -ExpandProperty 'commands' | 
+                    Where-Object { $_.name -eq $commandPreElements[0] } | 
+                    Select-Object -ExpandProperty 'definition' | 
+                    Select-Object -ExpandProperty 'options' | 
+                    ForEach-Object { $_.psobject.properties.Value } | 
+                    ForEach-Object { 
+                        $description = $_.description; 
+                        @{
+                            name        = $_.name; 
+                            description = $description 
+                        };
+                        
+                        $_.shortcut -split '\|' | 
+                            Where-Object { $_ -ne '' } | 
+                            ForEach-Object { 
+                                @{
+                                    name        = $_; 
+                                    description = $description 
+                                } 
+                            } 
+                        } | 
+                        Where-Object { $_.name -like "$wordToComplete*" } |
+                        ForEach-Object { 
+                            [System.Management.Automation.CompletionResult]::new(
+                                $_.name,
+                                $_.name,
+                                'ParameterName',
+                                $_.description)
+                        }
             
-        return
-    }
-}
+                        return
+                    }
+                }
+            }
